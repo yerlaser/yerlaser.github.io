59c618e94ad12a86c214cd2d68ec32015e9be8e5
Yerlaser <yerlaser@outlook.com>
2021-06-25 01:23:27 +0200

    zsh

--- configs/zshrc.sh
+++ configs/zshrc.sh
@@ -1,7 +1,7 @@
 HISTSIZE=1000
 SAVEHIST=1000
 HISTFILE=~/.history
-PROMPT='%F{magenta}%D{%d-%m %H:%M:%S} | %~ %#%f '
+PROMPT='%(?..[%?] )%F{magenta}%D{%d-%m %H:%M:%S} | %~ '
 DIRSTACKSIZE=7
 WORDCHARS=${WORDCHARS/\/}
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
@@ -10,10 +10,13 @@ autoload -Uz compinit && compinit
 autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
-bindkey -M viins "^[[3;2~" backward-kill-word
-bindkey -M viins "^[[Z" history-beginning-search-backward-end
 
-bindkey -v
+bindkey "^[" end-of-line
+bindkey "^[[3;2~" backward-kill-word
+bindkey "^[[Z" history-beginning-search-backward-end
+
+# bindkey -v
+# bindkey -M viins "^[[3;2~" backward-kill-word
 
 setopt auto_pushd autocd extended_history extendedglob hist_expire_dups_first hist_ignore_space hist_no_store hist_reduce_blanks hist_verify ksh_arrays nomatch notify share_history
 
