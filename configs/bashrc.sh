ab64091280167fbdbde5e39329c879ad0dbd89d6
Yerlaser <yerlaser@outlook.com>
2021-06-25 01:12:21 +0200

    bash

--- configs/bashrc.sh
+++ configs/bashrc.sh
@@ -1,13 +1,17 @@
-set -o vi
+# set -o vi; bind "set show-mode-in-prompt on"
 # bind -m vi-insert '"\e\r":"$(sk -m --bind 'tab:down,btab:up,alt-enter:toggle')\r"'
+alias '..'='cd ..'
+bind TAB:menu-complete
 bind "set completion-ignore-case on"
 bind "set menu-complete-display-prefix on"
 bind "set show-all-if-ambiguous on"
-bind "set show-mode-in-prompt on"
-bind TAB:menu-complete
-bind -m vi-insert '"\e[Z":history-search-backward'
-bind -m vi-insert '"\e[3;2~":backward-kill-word'
-# bind -M viins "^[[Z" history-beginning-search-backward-end
+bind '"\C-\xff":shell-expand-line'
+bind '"\e ":"\C-\xff\e[F"'
+bind '"\e":end-of-line'
+bind '"\e[Z":history-search-backward'
+bind '"\e[3;2~":backward-kill-word'
+PROMPT_COMMAND='MYVAR="$?" && [ "$MYVAR" -ne 0 ] && printf "[$MYVAR] "'
+PS1='\e[2;35m\D{%d-%m %H:%M:%S} | \w \e[0m'
 
 shopt -s direxpand
 shopt -s dotglob
