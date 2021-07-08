897189800ef92e306a262122bddae8c2b4d852f3
Yerlaser <yerlaser@outlook.com>
2021-07-08 22:25:23 +0200

    minor

--- configs/bashrc.sh
+++ configs/bashrc.sh
@@ -3,13 +3,13 @@ bind TAB:menu-complete
 bind "set completion-ignore-case on"
 bind "set menu-complete-display-prefix on"
 bind "set show-all-if-ambiguous on"
-bind '"\C-\xff":history-expand-line'
-bind '"\e[1;5F":"\C-\xff\e[F"'
-bind '"\e[1;5H":"\C-\xff\e[H"'
-bind '"\e ":"\C-\xff "'
+bind '"\C-@":history-expand-line'
+bind '"\e ":"\C-@ "'
+bind '"\e[1;5F":"\C-@\e[F"'
+bind '"\e[1;5H":"\C-@\e[H"'
 bind '"\e[13;2u":history-search-forward'
-bind '"\e[Z":history-search-backward'
 bind '"\e[3;2~":backward-kill-word'
+bind '"\e[Z":history-search-backward'
 PROMPT_COMMAND='MYVAR="$?" && [ "$MYVAR" -ne 0 ] && printf "[$MYVAR] "'
 PS1='\e[2;35m\D{%d-%m %H:%M:%S} | \w \e[0m'
 
