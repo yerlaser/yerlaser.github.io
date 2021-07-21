276f15d008b0b0d08de2fa4da0b5242bd9d67034
Yerlaser <yerlaser@outlook.com>
2021-07-21 23:20:33 +0200

    minor

--- configs/gtr/gtr.go
+++ configs/gtr/gtr.go
@@ -9,24 +9,24 @@ import (
 )
 
 func main() {
-	command := flag.NewFlagSet("align", flag.ExitOnError)
-	side := command.String("side", "", "Side (edge) of alignment (left, center, right)")
+	command := flag.NewFlagSet("center", flag.ExitOnError)
 	delim := command.String("delim", "", "Delimiter to fill padding")
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Error: command required")
+		os.Exit(1)
+	}
 	switch os.Args[1] {
-	case "align":
+	case "center":
 		command.Parse(os.Args[2:])
-		switch *side {
-		case "center":
-			msg := strings.Join(command.Args(), " ")
-			message := strings.Trim(msg, "\r\n")
-			eol := strings.TrimPrefix(msg, message)
-			width, _, _ := term.GetSize(0)
-			paddingLeft := (width - len(message)) / 2
-			paddingRight := paddingLeft
-			if (paddingLeft + paddingRight + len(message)) < width {
-				paddingRight += 1
-			}
-			fmt.Printf("%s%s%s%s", strings.Repeat(*delim, paddingLeft), message, strings.Repeat(*delim, paddingRight), eol)
+		msg := strings.Join(command.Args(), " ")
+		message := strings.Trim(msg, "\r\n")
+		eol := strings.TrimPrefix(msg, message)
+		width, _, _ := term.GetSize(0)
+		paddingLeft := (width - len(message)) / 2
+		paddingRight := paddingLeft
+		if (paddingLeft + paddingRight + len(message)) < width {
+			paddingRight += 1
 		}
+		fmt.Printf("%s%s%s%s", strings.Repeat(*delim, paddingLeft), message, strings.Repeat(*delim, paddingRight), eol)
 	}
 }
