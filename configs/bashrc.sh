855e3232ca5719aec3d7ea1b907d22f8d11cda72
Yerlaser <yerlaser@outlook.com>
2021-07-28 00:53:32 +0200

    fixed bash prompt

--- configs/bashrc.sh
+++ configs/bashrc.sh
@@ -5,13 +5,14 @@ bind "set menu-complete-display-prefix on"
 bind "set show-all-if-ambiguous on"
 bind '"\C-@":history-expand-line'
 bind '"\e ":" "'
-bind '"\e[1;5F":"\C-@\e[F"'
-bind '"\e[1;5H":"\C-@\e[H"'
-bind '"\e[13;2u":history-search-forward'
+bind '"\e[1;2C":beginning-of-line'
+bind '"\e[1;5C":end-of-line'
+bind '"\e[1;5D":history-search-forward'
 bind '"\e[3;2~":backward-kill-word'
+bind '"\e[3;6~":unix-filename-rubout'
 bind '"\e[Z":history-search-backward'
-# PROMPT_COMMAND='MYVAR="$?" && [ "$MYVAR" -ne 0 ] && printf "[$MYVAR] "'
-# PS1='\e[2;35m\D{%d-%m %H:%M:%S} | \w \e[0m'
+PROMPT_COMMAND='MYVAR="$?" && [ "$MYVAR" -ne 0 ] && printf "[$MYVAR] "'
+PS1="\[\e[2;35m\]\D{%d-%m %H:%M:%S} | \w \[\e[0m\]"
 
 shopt -s direxpand
 shopt -s dotglob
