0b7baa1e74d963d3d37de337595a4493c575a43f
Yerlaser <yerlaser@outlook.com>
2021-11-20 00:38:05 +0100

    zsh

--- configs/shrc.sh
+++ configs/shrc.sh
@@ -5,6 +5,6 @@ alias dirsize='ls -FAhlrS'
 
 if command -v dnf &> /dev/null; then alias dnf='dnf --cacheonly'; fi
 if command -v podman &> /dev/null; then alias docker=podman; fi
-if command -v kak &> /dev/null; then alias kakn='kak -n'; alias vi=kak; fi
+if command -v kak &> /dev/null; then alias kakn='kak -n'; alias vi=kak; alias view='kak -ro'; fi
 
 set -o noclobber
--- configs/zshrc.sh
+++ configs/zshrc.sh
@@ -1,23 +1,13 @@
+source /LOCAL/Sources/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+source /LOCAL/Published/configs/shrc.sh
+
 HISTSIZE=1000
 SAVEHIST=1000
 HISTFILE=~/.history
 PROMPT='%(?..[%?] )%F{magenta}%D{%d-%m %H:%M:%S} | %~ '
-DIRSTACKSIZE=7
 WORDCHARS=${WORDCHARS/\/}
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-autoload -Uz compinit && compinit
 
-autoload -U history-search-end
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
+zstyle ':autocomplete:*' min-input 1
+zstyle ':completion:*:paths' path-completion yes
 
-bindkey "^[" end-of-line
-bindkey "^[[3;2~" backward-kill-word
-bindkey "^[[Z" history-beginning-search-backward-end
-
-# bindkey -v
-# bindkey -M viins "^[[3;2~" backward-kill-word
-
-setopt auto_pushd autocd extended_history extendedglob hist_expire_dups_first hist_ignore_space hist_no_store hist_reduce_blanks hist_verify ksh_arrays nomatch notify share_history
-
-source /LOCAL/Published/configs/shrc.sh
+setopt hist_ignore_all_dups hist_ignore_space hist_no_store hist_reduce_blanks hist_verify share_history
