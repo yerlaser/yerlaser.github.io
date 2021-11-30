2cc30dc4253efd98d3795aaba17abb3f6cd306d9
Yerlaser <yerlaser@outlook.com>
2021-11-30 21:24:17 +0100

    zbsh

--- configs/bashrc.sh
+++ configs/bashrc.sh
@@ -1,16 +1,16 @@
-alias '..'='cd ..'
 bind TAB:menu-complete
 bind "set completion-ignore-case on"
 bind "set menu-complete-display-prefix on"
 bind "set show-all-if-ambiguous on"
+
 bind '"\C-@":history-expand-line'
-bind '"\e ":" "'
-bind '"\e[1;2C":beginning-of-line'
-bind '"\e[1;5C":end-of-line'
-bind '"\e[1;5D":history-search-forward'
-bind '"\e[3;2~":backward-kill-word'
-bind '"\e[3;6~":unix-filename-rubout'
-bind '"\e[Z":history-search-backward'
+bind '"\e[A":history-search-backward'
+bind '"\e[B":history-search-forward'
+bind '"\e[5~":backward-word'
+bind '"\e[6~":forward-word'
+bind '"\e[17~":kill-line'
+bind '"\e[19~":kill-whole-line'
+bind '"\eOR":backward-kill-line'
 PROMPT_COMMAND='MYVAR="$?" && [ "$MYVAR" -ne 0 ] && printf "[$MYVAR] "'
 PS1="\[\e[2;35m\]\D{%d-%m %H:%M:%S} | \w \[\e[0m\]"
 
@@ -22,3 +22,8 @@ shopt -s histverify
 HISTCONTROL=ignoredups:erasedups:ignorespace
 
 source /LOCAL/Published/configs/shrc.sh
+if [ -s "$HOME/Sources/bash-complete-partial-path/bash_completion" ]
+then
+    source "$HOME/Sources/bash-complete-partial-path/bash_completion"
+    _bcpp --defaults
+fi
--- configs/zshrc.sh
+++ configs/zshrc.sh
@@ -1,4 +1,4 @@
-source /LOCAL/Sources/zsh-autosuggestions/zsh-autosuggestions.zsh
+# source /LOCAL/Sources/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /LOCAL/Published/configs/shrc.sh
 
 HISTFILE=~/.history
@@ -6,20 +6,25 @@ HISTSIZE=1000
 PROMPT='%(?..[%?] )%F{magenta}%D{%d-%m %H:%M:%S} | %~ %f'
 SAVEHIST=1000
 WORDCHARS=${WORDCHARS/\/}
-ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=()
-ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(end-of-line forward-char forward-word)
-ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+# ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=()
+# ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(end-of-line forward-char forward-word)
+# ZSH_AUTOSUGGEST_STRATEGY=(completion history)
 
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 autoload -Uz compinit && compinit
+# autoload -U history-search-end
+# zle -N history-beginning-search-backward-end history-search-end
 
+bindkey '\C-@' expand-history
+bindkey '\e[A' history-beginning-search-backward
+bindkey '\e[B' history-beginning-search-forward
 bindkey '\e[5~' backward-word
 bindkey '\e[6~' forward-word
 bindkey '\e[17~' kill-line
-bindkey '\e[19~' backward-kill-line
+bindkey '\e[19~' kill-whole-line
+bindkey '\eOR' backward-kill-line
 bindkey '\e[F' end-of-line
 bindkey '\e[H' beginning-of-line
-bindkey '\eOR' kill-whole-line
 
 setopt hist_ignore_all_dups hist_ignore_space hist_no_store hist_reduce_blanks hist_verify menu_complete no_list_ambiguous share_history
