1b387adb70dcba116ce488c8b01991e56dc08ae1
Yerlaser <yerlaser@outlook.com>
2021-12-21 21:42:58 +0100

    completion

--- configs/zshrc.sh
+++ configs/zshrc.sh
@@ -1,4 +1,6 @@
-# source /LOCAL/Sources/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /LOCAL/Sources/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /LOCAL/Sources/zsh-history-substring-search/zsh-history-substring-search.zsh
+
 source /LOCAL/Published/configs/shrc.sh
 
 HISTFILE=~/.history
@@ -6,22 +8,20 @@ HISTSIZE=1000
 PROMPT='%(?..[%?] )%F{magenta}%D{%d-%m %H:%M:%S} | %~ %f'
 SAVEHIST=1000
 WORDCHARS=${WORDCHARS/\/}
-# ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=()
-# ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(end-of-line forward-char forward-word)
+ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=()
+ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(end-of-line forward-char forward-word)
 # ZSH_AUTOSUGGEST_STRATEGY=(completion history)
 
-zstyle ':completion:' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|= r:|=' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|= r:|=' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|= r:|=*'
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Za-z}'
 
 autoload -Uz compinit && compinit
-# autoload -U history-search-end
-# zle -N history-beginning-search-backward-end history-search-end
 
 bindkey '\C-U' backward-kill-line
 bindkey '\e^' expand-history
 bindkey '\e[5~' vi-backward-blank-word
 bindkey '\e[6~' vi-forward-blank-word
-bindkey '\e[A' history-beginning-search-backward
-bindkey '\e[B' history-beginning-search-forward
+bindkey '\e[A' history-substring-search-up
+bindkey '\e[B' history-substring-search-down
 bindkey '\e[F' end-of-line
 bindkey '\e[H' beginning-of-line
 
