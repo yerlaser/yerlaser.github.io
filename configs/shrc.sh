8d91a838767d80d7fc828ce29a58de2d6673fe47
Yerlaser <yerlaser@outlook.com>
2022-02-16 23:50:42 +0100

    Paths to shrc

--- configs/shrc.sh
+++ configs/shrc.sh
@@ -1,3 +1,45 @@
+# export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/LOCAL/apps/gcc/include/c++/12.0.0"
+# export LD_LIBRARY_PATH="/LOCAL/apps/gcc/lib64:$LD_LIBRARY_PATH"
+# export LD_RUN_PATH="/LOCAL/apps/gcc/lib64:$LD_LIBRARY_PATH"
+# export CC="/LOCAL/apps/gcc/bin/gcc"
+# export CXX="/LOCAL/apps/gcc/bin/g++"
+
+export CPLUS_INCLUDE_PATH="/LOCAL/apps/clang/include/c++/v1"
+export LD_LIBRARY_PATH="/LOCAL/apps/clang/lib64"
+export LD_RUN_PATH="/LOCAL/apps/clang/lib64"
+export CC="/LOCAL/apps/clang/bin/clang"
+export CXX="/LOCAL/apps/clang/bin/clang++"
+
+export MANPATH=":/LOCAL/apps/fish/share/man"
+export MANPATH=":/LOCAL/apps/gcc/share/man"
+export MANPATH=":/LOCAL/apps/git/share/man"
+export MANPATH=":/LOCAL/apps/jdk/man"
+export MANPATH=":/LOCAL/apps/kak/share/man"
+export MANPATH=":/LOCAL/apps/clang/share/man"
+export MANPATH=":/LOCAL/apps/node/share/man"
+export MANPATH=":/LOCAL/apps/ruby/share/man"
+
+export GIT_PAGER=less
+# export GDK_BACKEND=wayland
+# export QT_QPA_PLATFORM=wayland
+# export SDL_VIDEODRIVER=wayland
+
+# export GOPATH="/BUILD/go"
+# export RUSTFLAGS="-L /LOCAL/apps/rust/rust-std-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/lib"
+export PYTHONPYCACHEPREFIX="$HOME/.local/var/python"
+
+PATH="$PATH:/BUILD/node/bin"
+PATH="$PATH:/LOCAL/apps/cmake/bin"
+PATH="$PATH:/LOCAL/apps/fish/bin"
+# PATH="/LOCAL/apps/gcc/bin:$PATH"
+PATH="$PATH:/LOCAL/apps/git/bin"
+PATH="$PATH:/LOCAL/apps/jdk/bin"
+PATH="$PATH:/LOCAL/apps/kak/bin"
+PATH="/LOCAL/apps/clang/bin:$PATH"
+PATH="$PATH:/LOCAL/apps/mruby/bin"
+PATH="$PATH:/LOCAL/apps/node/bin"
+PATH="$PATH:/LOCAL/apps/ruby/bin"
+
 alias '..'='cd ..'
 alias '...'='cd ../..'
 alias dir='ls -FAhl'
