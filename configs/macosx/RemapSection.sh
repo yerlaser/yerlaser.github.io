f099d79dbe5599999aec6341fdce64aca41baa5b
Yerlaser <yerlaser@outlook.com>
2022-04-24 20:22:57 +0200

    returnus

--- configs/macosx/RemapSection.sh
+++ configs/macosx/RemapSection.sh
@@ -1,1 +1,1 @@
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000064}]}'
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E7}]}'
