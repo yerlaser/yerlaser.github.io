259088e69124b280864fb6939e0e62e259fbd844
Yerlan <yerlaser@gmail.com>
2022-06-09 22:38:43 +0200

    Update zshrc.sh

--- configs/zshrc.sh
+++ configs/zshrc.sh
@@ -1,42 +1,24 @@
-function addToPATH {
-  case ":$PATH:" in
-    *":$1:"*) :;;
-    *) PATH="$1:$PATH";;
-  esac
-}
-
-addToPATH "$HOME/Downloads/kotlinc/bin"
-addToPATH "/LOCAL/apps/clang/bin"
-addToPATH "/LOCAL/apps/ruby/bin"
-
-# export CPLUS_INCLUDE_PATH="/LOCAL/apps/gcc/include/c++/12.0.0"
-# export LD_LIBRARY_PATH="/LOCAL/apps/gcc/lib64"
-# export LD_RUN_PATH="/LOCAL/apps/gcc/lib64"
-# export CC="/LOCAL/apps/gcc/bin/gcc"
-# export CXX="/LOCAL/apps/gcc/bin/g++"
-
-# export CPLUS_INCLUDE_PATH="/LOCAL/apps/clang/include/c++/v1"
-# export LD_LIBRARY_PATH="/LOCAL/apps/clang/lib64"
-# export LD_RUN_PATH="/LOCAL/apps/clang/lib64"
-# export CC="/LOCAL/apps/clang/bin/clang"
-# export CXX="/LOCAL/apps/clang/bin/clang++"
-
-# export MANPATH=":/LOCAL/apps/jdk/man"
-export GIT_PAGER=less
-
-# export PYTHONPYCACHEPREFIX="$HOME/.local/var/python"
-
-alias '..'='cd ..'
-alias '...'='cd ../..'
-alias dir='ls -FAhl'
-alias dirtime='ls -FAhlrt'
-alias dirsize='ls -FAhlrS'
-
-if command -v dnf &> /dev/null; then alias dnf='dnf --cacheonly'; fi
-if command -v podman &> /dev/null; then alias docker=podman; fi
-
-alias view='vi -ro'
-export EDITOR=vi
-export VISUAL=vi
-
-set -o noclobber
+alias dh='dirs -v'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -3'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+alias 0='cd -10'
+source ~/Published/configs/shrc.sh
+DIRSTACKSIZE=11
+HISTFILE=~/.history
+HISTSIZE=1000
+PROMPT='%(?..[%?] )%F{magenta}%D{%d-%m %H:%M:%S} | %~ %f'
+SAVEHIST=1000
+WORDCHARS=${WORDCHARS/\/}
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Za-z}'
+autoload -Uz compinit && compinit
+bindkey '^[[34~' expand-or-complete
+bindkey '^N' history-beginning-search-forward
+bindkey '^P' history-beginning-search-backward
+setopt autocd autopushd hist_ignore_all_dups hist_ignore_space hist_no_store hist_reduce_blanks hist_verify menu_complete no_list_ambiguous share_history pushdignoredups pushdminus pushdsilent pushdtohome
