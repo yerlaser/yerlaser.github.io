7f4e67dd4cce4a2d3fb66c4840ef829c7bc39b6f
Yerlan <yerlaser@gmail.com>
2022-06-09 22:30:37 +0200

    Update zshrc.sh

--- configs/zshrc.sh
+++ configs/zshrc.sh
@@ -1,11 +1,42 @@
-source ~/Published/configs/shrc.sh
-HISTFILE=~/.history
-HISTSIZE=1000
-PROMPT='%(?..[%?] )%F{magenta}%D{%d-%m %H:%M:%S} | %~ %f'
-SAVEHIST=1000
-WORDCHARS=${WORDCHARS/\/}
-zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Za-z}'
-autoload -Uz compinit && compinit
-bindkey '\e[A' history-beginning-search-backward
-bindkey '\e[B' history-beginning-search-forward
-setopt hist_ignore_all_dups hist_ignore_space hist_no_store hist_reduce_blanks hist_verify menu_complete no_list_ambiguous share_history
+function addToPATH {
+  case ":$PATH:" in
+    *":$1:"*) :;;
+    *) PATH="$1:$PATH";;
+  esac
+}
+
+addToPATH "$HOME/Downloads/kotlinc/bin"
+addToPATH "/LOCAL/apps/clang/bin"
+addToPATH "/LOCAL/apps/ruby/bin"
+
+# export CPLUS_INCLUDE_PATH="/LOCAL/apps/gcc/include/c++/12.0.0"
+# export LD_LIBRARY_PATH="/LOCAL/apps/gcc/lib64"
+# export LD_RUN_PATH="/LOCAL/apps/gcc/lib64"
+# export CC="/LOCAL/apps/gcc/bin/gcc"
+# export CXX="/LOCAL/apps/gcc/bin/g++"
+
+# export CPLUS_INCLUDE_PATH="/LOCAL/apps/clang/include/c++/v1"
+# export LD_LIBRARY_PATH="/LOCAL/apps/clang/lib64"
+# export LD_RUN_PATH="/LOCAL/apps/clang/lib64"
+# export CC="/LOCAL/apps/clang/bin/clang"
+# export CXX="/LOCAL/apps/clang/bin/clang++"
+
+# export MANPATH=":/LOCAL/apps/jdk/man"
+export GIT_PAGER=less
+
+# export PYTHONPYCACHEPREFIX="$HOME/.local/var/python"
+
+alias '..'='cd ..'
+alias '...'='cd ../..'
+alias dir='ls -FAhl'
+alias dirtime='ls -FAhlrt'
+alias dirsize='ls -FAhlrS'
+
+if command -v dnf &> /dev/null; then alias dnf='dnf --cacheonly'; fi
+if command -v podman &> /dev/null; then alias docker=podman; fi
+
+alias view='vi -ro'
+export EDITOR=vi
+export VISUAL=vi
+
+set -o noclobber
