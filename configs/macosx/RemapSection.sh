18aaa31d0775dc650bf72d64dcbe5427aa9ce719
Yerlan <yerlaser@gmail.com>
2022-06-09 22:33:52 +0200

    Update RemapSection.sh

--- configs/macosx/RemapSection.sh
+++ configs/macosx/RemapSection.sh
@@ -1,5 +1,7 @@
 hidutil property --set '{"UserKeyMapping":[
   {"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035},
-  {"HIDKeyboardModifierMappingSrc":0x7000000E7,"HIDKeyboardModifierMappingDst":0x7000000E3},
-  {"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E7}
+  {"HIDKeyboardModifierMappingSrc":0x7000000E4,"HIDKeyboardModifierMappingDst":0x7000000E0},
+  {"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E4},
+  {"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":0x70000002B},
+  {"HIDKeyboardModifierMappingSrc":0x70000002B,"HIDKeyboardModifierMappingDst":0x70000004F}
 ]}'
