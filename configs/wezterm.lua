1345b66f76d9dad5dcee55ad14e99f8b061ed0a8
Yerlan <yerlaser@gmail.com>
2022-07-07 02:20:34 +0200

    Update wezterm.lua

--- configs/wezterm.lua
+++ configs/wezterm.lua
@@ -1,10 +1,13 @@
-local wezterm = require 'wezterm';
+local wezterm = require 'wezterm'
+
 return {
-  font = wezterm.font('Monaco'),
-  font_size = 19.0,
-  keys = {
-    {key="Backspace", mods="SHIFT", action=wezterm.action.SendString("\x1b[1;2D")}
-  },
-  default_prog = {"/Users/yerlan.sergaziyev/Sources/nushell/target/release/nu"},
+  color_scheme = "Dracula+",
   default_cwd = "/Users/yerlan.sergaziyev/Gerrit",
+  default_prog = {"/Users/yerlan.sergaziyev/Sources/nushell/target/release/nu"},
+  font = wezterm.font('JetBrains Mono'),
+  font_size = 23,
+  keys = {
+    { key = "Backspace", mods = "SHIFT", action = wezterm.action.SendString("\x1b[1;2D") }
+  },
+  window_frame = { font_size = 21 }
 }
