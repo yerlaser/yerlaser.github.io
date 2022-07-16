218bdd940fdf14b13fbaf1199c11c4b403517d7a
Yerlan <yerlaser@gmail.com>
2022-07-16 22:49:43 +0200

    Update wezterm.lua

--- configs/wezterm.lua
+++ configs/wezterm.lua
@@ -1,5 +1,4 @@
 local wezterm = require 'wezterm'
-
 return {
   color_scheme = "Dracula+",
   default_cwd = "/Users/yerlan.sergaziyev/Gerrit",
@@ -7,7 +6,11 @@ return {
   font = wezterm.font('JetBrains Mono'),
   font_size = 23,
   keys = {
-    { key = "Backspace", mods = "SHIFT", action = wezterm.action.SendString("\x1b[1;2D") }
+    { key = "Backspace", mods = "SHIFT", action = wezterm.action.SendString("\x18") }, -- Ctrl-x
+    { key = "End", mods = "", action = wezterm.action.SendString("\x03") }, -- Ctrl-c
+    { key = "End", mods = "SHIFT", action = wezterm.action.SendString("\x01") }, -- Ctrl-a
+    { key = "Enter", mods = "SHIFT", action = wezterm.action.SendString("\x05") }, -- Ctrl-e
+    { key = "Space", mods = "SHIFT", action = wezterm.action.SendString("\x12") }, -- Ctrl-r
   },
   window_frame = { font_size = 21 }
 }
