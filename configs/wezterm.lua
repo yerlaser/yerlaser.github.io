f41ea7313dab6e698be72ef5cb3dfea0162c9749
Yerlan <yerlaser@gmail.com>
2022-07-18 22:05:50 +0200

    Update wezterm.lua

--- configs/wezterm.lua
+++ configs/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+
 return {
   color_scheme = "Ryuuko",
   default_cwd = "/Users/yerlan.sergaziyev/Gerrit",
@@ -6,11 +7,12 @@ return {
   font = wezterm.font('JetBrains Mono'),
   font_size = 23,
   keys = {
-    { key = "Backspace", mods = "SHIFT", action = wezterm.action.SendString("\x18") }, -- Ctrl-x
+    { key = "Backspace", mods = "SHIFT", action = wezterm.action.SendString("\x12") }, -- Ctrl-r
     { key = "End", mods = "", action = wezterm.action.SendString("\x03") }, -- Ctrl-c
     { key = "End", mods = "SHIFT", action = wezterm.action.SendString("\x01") }, -- Ctrl-a
     { key = "Enter", mods = "SHIFT", action = wezterm.action.SendString("\x05") }, -- Ctrl-e
-    { key = "Space", mods = "SHIFT", action = wezterm.action.SendString("\x12") }, -- Ctrl-r
+    { key = "Space", mods = "SHIFT", action = wezterm.action.SendString("\x18") }, -- Ctrl-x
   },
-  window_frame = { font_size = 21 }
+  window_frame = { font_size = 24 },
+  window_decorations = "RESIZE"
 }
