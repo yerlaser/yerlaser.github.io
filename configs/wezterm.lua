ed1e848bd8be66ca52ea61c4055fe42aa54e0e3b
Yerlan <yerlaser@gmail.com>
2022-08-07 16:33:34 +0200

    Update wezterm.lua

--- configs/wezterm.lua
+++ configs/wezterm.lua
@@ -11,7 +11,7 @@ return {
     { key = "End", mods = "", action = wezterm.action.SendString("\x1B") }, -- Esc
     { key = "End", mods = "SHIFT", action = wezterm.action.SendString("\x1B_") }, -- Alt-_
     { key = "Enter", mods = "SHIFT", action = wezterm.action.SendString("\x1Bf") }, -- Alt-f
-    { key = "Space", mods = "SHIFT", action = wezterm.action.SendString("\x1B\x05") }, -- Alt-Ctrl-e
+    { key = "Space", mods = "SHIFT", action = wezterm.action.SendString("\x1B^") }, -- Alt-^
   },
   window_frame = { font_size = 24 },
   window_decorations = "RESIZE"
