71b5d77919905c00a5cb04b7fead00a8cf22532c
Yerlan <yerlaser@gmail.com>
2022-09-19 23:56:15 +0200

    Move button to a component

--- configs/sycamore/main.rs
+++ configs/sycamore/main.rs
@@ -1,37 +1,30 @@
 use sycamore::prelude::*;
+mod button;
 mod count;
 mod props;
 mod square;
 
-// Create initial empty file:
-//     touch tailwind.css
-// Add following line to the index.html:
-//     <link data-trunk href="./tailwind.css" rel="css" />
-
-#[component]
-fn App<G: Html>(cx: Scope) -> View<G> {
-    let state = create_signal(cx, 0i32);
-    let increment = |_| state.set(*state.get() + 1);
-    let decrement = |_| state.set(*state.get() - 1);
-    let reset = |_| state.set(0);
-
-    view! { cx,
-        article(class="flex flex-col justify-center items-center") {
-            header {
-                button(on:click=decrement, class="h-12 px-6 m-2 text-lg text-red-100 transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline hover:bg-red-800") { "-" }
-                button(on:click=reset, class="h-12 px-6 m-2 text-lg text-yellow-100 transition-colors duration-150 bg-yellow-700 rounded-lg focus:shadow-outline hover:bg-yellow-800") { "0" }
-                button(on:click=increment, class="h-12 px-6 m-2 text-lg text-green-100 transition-colors duration-150 bg-green-700 rounded-lg focus:shadow-outline hover:bg-green-800") { "+" }
-            }
-            count::Count(value=state)
-            square::Square(value=state)
-        }
-    }
-}
-
 fn main() {
     sycamore::render(|cx| {
+        let state = create_signal(cx, 0i32);
+        let update = |action| {
+            if action == 0 {
+                state.set(0);
+            } else {
+                state.set(*state.get() + action);
+            }
+        };
+        let update = create_ref(cx, update);
         view! { cx,
-            App {}
+            article(class="flex flex-col justify-center items-center") {
+                header {
+                    button::Button(updater=update, action=-1)
+                    button::Button(updater=update, action=0)
+                    button::Button(updater=update, action=1)
+                }
+                count::Count(value=state)
+                square::Square(value=state)
+            }
         }
     });
 }
