fe446941dce9b3ced9e63d61dc6e5e2dd2307fdf
Yerlan <yerlaser@gmail.com>
2022-09-19 23:56:59 +0200

    Move button to a component

--- configs/sycamore/props.rs
+++ configs/sycamore/props.rs
@@ -4,3 +4,9 @@ use sycamore::prelude::*;
 pub struct Base<'a> {
     pub value: &'a ReadSignal<i32>,
 }
+
+#[derive(Prop)]
+pub struct ButtonProps<'a> {
+    pub updater: &'a dyn Fn(i32),
+    pub action: i32,
+}
