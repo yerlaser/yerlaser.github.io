ba00b11464adef82fb144be5b089a45e8ab3e834
Yerlan <yerlaser@gmail.com>
2022-09-27 21:39:33 +0200

    Use inline props

--- configs/sycamore/props.rs
+++ configs/sycamore/props.rs
@@ -1,11 +1,6 @@
 use sycamore::prelude::*;
 
 #[derive(Prop)]
-pub struct Base<'a> {
-    pub value: &'a ReadSignal<i32>,
-}
-
-#[derive(Prop)]
 pub struct ButtonProps<'a> {
     pub updater: &'a dyn Fn(i32),
     pub action: i32,
