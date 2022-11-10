6ed3fb39cd617c2dbccf552dfffa0902c3d4da95
Yerlan <yerlaser@gmail.com>
2022-11-10 10:46:35 +0100

    Update wezterm.lua

--- configs/wezterm.lua
+++ configs/wezterm.lua
@@ -10,16 +10,14 @@ return {
   color_scheme = "Catppuccin Latte",
   default_cwd = "/Users/yerlan.sergaziyev/Gerrit",
   default_prog = {"/Users/yerlan.sergaziyev/.cargo/bin/nu", "--config", "/Users/yerlan.sergaziyev/Published/configs/configWez.nu"},
-  enable_tab_bar = false,
-  font = wezterm.font('JetBrains Mono'),
-  font_size = 23,
+  font = wezterm.font('Monaco'),
+  font_size = 17,
   keys = {
     { key = "Backspace", mods = "SHIFT", action = wezterm.action.SendString("\x1Bb") }, -- Alt-b
-    { key = "End", mods = "", action = wezterm.action.SendString("\x1B") }, -- Esc
-    { key = "End", mods = "SHIFT", action = wezterm.action.SendString("\x1B_") }, -- Alt-_
+    { key = "DownArrow", mods = "SHIFT", action = wezterm.action.SendString("\x1B_") }, -- Alt-_
     { key = "Enter", mods = "SHIFT", action = wezterm.action.SendString("\x1Bf") }, -- Alt-f
     { key = "Space", mods = "SHIFT", action = wezterm.action.SendString("\x1B^") }, -- Alt-^
   },
-  window_frame = { font_size = 23 },
+  window_frame = { font_size = 17 },
   window_decorations = "RESIZE"
 }
