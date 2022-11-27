5a8c28ecd5d050d087a7ce53c0469f6772530f8d
Yerlaser <yerlaser@outlook.com>
2022-11-27 23:43:59 +0600

    Added a couple of useful Rust functions

---
+++ configs/rstr.rs
@@ -0,0 +1,42 @@
+macro_rules! readln{
+    ($message: expr, $receiver: expr) => {
+        {
+            use std::io::{stdin, stdout, Write};
+
+            print!("{}", $message);
+            stdout().flush().expect("Error reading from stdio");
+            stdin().read_line($receiver).expect("Error flushing stdio");
+            *$receiver = $receiver.trim_end().to_string();
+        }
+    }
+}
+
+trait StringExt {
+    fn capitalize(&self) -> String;
+}
+
+impl StringExt for String {
+    fn capitalize(&self) -> String {
+        let mut c = self.chars();
+        match c.next() {
+            None => String::new(),
+            Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+        }
+    }
+}
+
+impl StringExt for &str {
+    fn capitalize(&self) -> String {
+        let mut c = self.chars();
+        match c.next() {
+            None => String::new(),
+            Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+        }
+    }
+}
+
+fn main() {
+    let mut name = String::new();
+    readln!("Enter your name: ", &mut name);
+    println!("{} {}!", "hello".capitalize(), name.capitalize());
+}
