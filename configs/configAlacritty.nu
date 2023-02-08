90fd39bddab4a8c2a3f75bc77613e460090b195c
Yerlan Sergaziyev <yerlaser@gmail.com>
2023-02-08 18:06:22 +0100

    fixed keyboard layout

--- configs/configAlacritty.nu
+++ configs/configAlacritty.nu
@@ -1,3 +1,5 @@
 source ~/Published/configs/config.nu
 alias jour = hx ~/journal.md
 # let-env config = ($env.config | upsert history {history_file_format: "sqlite"})
+
+source /Users/yerlan/.config/broot/launcher/nushell/br
--- configs/macosx/RemapSection.sh
+++ configs/macosx/RemapSection.sh
@@ -4,6 +4,6 @@
 
 hidutil property --set '{"UserKeyMapping":[
   {"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035},
-  {"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000051},
-  {"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000052}
+  {"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000004C},
+  {"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x70000004D}
 ]}'
