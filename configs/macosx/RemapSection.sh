7487a8ef28bb098d3639b669e1e29f9431b595c8
Yerlan Sergaziyev <yerlaser@gmail.com>
2023-03-14 10:52:12 +0100

    fixed left right control

--- configs/macosx/RemapSection.sh
+++ configs/macosx/RemapSection.sh
@@ -6,8 +6,8 @@
   # {"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x70000004D}
 
 hidutil property --set '{"UserKeyMapping":[
-  {"HIDKeyboardModifierMappingSrc":0x7000000E7,"HIDKeyboardModifierMappingDst":0x7000000E0},
-  {"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":0x7000000E7},
+  {"HIDKeyboardModifierMappingSrc":0x7000000E7,"HIDKeyboardModifierMappingDst":0x7000000E4},
+  {"HIDKeyboardModifierMappingSrc":0x7000000E4,"HIDKeyboardModifierMappingDst":0x7000000E7},
   {"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035},
   {"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000004C}
 ]}'
