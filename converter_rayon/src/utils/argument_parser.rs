6ced8f62ad4d78c3c440433b1d452355da40dbdb
Yerlan Sergaziyev <yerlaser@gmail.com>
2023-11-05 22:54:10 +0600

    removed defaults

--- converter_rayon/src/utils/argument_parser.rs
+++ converter_rayon/src/utils/argument_parser.rs
@@ -3,10 +3,10 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None)]
 pub struct CliArgs {
-    #[arg(short, long, default_value_t = String::from("conversion_table.csv"))]
+    #[arg(short, long)]
     pub table: String,
 
-    #[arg(short, long, default_value_t = String::from("lat-"))]
+    #[arg(short, long, default_value_t = String::new())]
     pub prefix: String,
 
     #[arg(short, long, default_value_t = String::new())]
