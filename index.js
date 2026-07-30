0032a06730a92071285001a31452a82037256556
Yerlan Sergaziyev <yerlaser@gmail.com>
2026-07-31 00:52:17 +0200

    [kz] moved to wasm-bindgen

---
+++ index.js
@@ -0,0 +1,57 @@
+import wasmInit, {convert} from './l2c.js'
+const rustWasm = await wasmInit('./l2c_bg.wasm')
+
+const src = document.getElementById('source')
+const res = document.getElementById('result')
+const lan = document.getElementById('lang')
+const mkp = document.getElementById('markup')
+const btn = document.getElementById('copy_or_more')
+
+src.addEventListener('change', (ev) => {
+  handleChange()
+})
+
+lan.addEventListener('change', (ev) => {
+  handleChange()
+})
+
+mkp.addEventListener('change', (ev) => {
+  handleChange()
+})
+
+btn.addEventListener('click', (ev) => {
+  handleKey('clip')
+})
+
+btn.addEventListener('keydown', (ev) => {
+  ev.preventDefault()
+  if (ev.key === 'Backspace') {
+    handleKey('clear')
+  } else if (ev.key === 'Tab') {
+    handleKey('insert')
+  } else if (ev.key === 'Enter') {
+    handleKey('clip')
+  } else if (ev.key === ' ') {
+    handleKey()
+  }
+})
+
+function handleChange() {
+  res.value = convert(src.value, lan.value, mkp.value)
+}
+
+function handleKey(action = 'return') {
+  if (action === 'clip') {
+    res.select()
+    document.execCommand("copy")
+  }
+
+  if (action === 'clear') {
+    src.value = ''
+    src.dispatchEvent(new InputEvent('change', {bubbles: true}))
+  } else if (action === 'insert') {
+    src.setRangeText('\t', src.selectionStart, src.selectionEnd, 'end')
+  }
+
+  src.focus()
+}
