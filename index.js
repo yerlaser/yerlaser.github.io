8b9c800c8c50e739a994a8bc3db6ff932ed8d944
Yerlan Sergaziyev <yerlaser@gmail.com>
2026-08-03 09:50:21 +0200

    [kz] improved ui

--- index.js
+++ index.js
@@ -2,9 +2,44 @@ import wasmInit, {Converter} from './l2c.js'
 const rustWasm = await wasmInit('./l2c_bg.wasm')
 const converter = new Converter()
 
-const btn = document.getElementById('copy_or_more')
+const cpy = document.getElementById('copy')
+const clr = document.getElementById('clear')
+const ins = document.getElementById('insert')
+
+cpy.addEventListener('click', (ev) => {
+  res.select()
+  document.execCommand("copy")
+  src.focus()
+})
+
+clr.addEventListener('click', (ev) => {
+  src.value = ''
+  src.focus()
+})
+
+ins.addEventListener('click', (ev) => {
+  src.setRangeText('\t', src.selectionStart, src.selectionEnd, 'end')
+  src.focus()
+})
+
 const lan = document.getElementById('lang')
 const mkp = document.getElementById('markup')
+
+function handleChange() {
+  res.value = converter.convert(src.value, lan.value, mkp.value)
+  src.focus()
+}
+
+lan.addEventListener('change', (ev) => {
+  handleChange()
+  src.focus()
+})
+
+mkp.addEventListener('change', (ev) => {
+  handleChange()
+  src.focus()
+})
+
 const res = document.getElementById('result')
 const src = document.getElementById('source')
 
@@ -20,53 +55,8 @@ function debounce(callback, delay) {
 
 const handleInput = debounce((e) => {
   handleChange()
-}, 700)
-
-function handleChange() {
-  res.value = converter.convert(src.value, lan.value, mkp.value)
-}
-
-function handleKey(action = 'return') {
-  if (action === 'clip') {
-    res.select()
-    document.execCommand("copy")
-  }
-
-  if (action === 'clear') {
-    src.value = ''
-    src.dispatchEvent(new InputEvent('change', {bubbles: true}))
-  } else if (action === 'insert') {
-    src.setRangeText('\t', src.selectionStart, src.selectionEnd, 'end')
-  }
-
-  src.focus()
-}
+}, 300)
 
 src.addEventListener('input', (ev) => {
   handleInput()
-})
-
-lan.addEventListener('change', (ev) => {
-  handleChange()
-})
-
-mkp.addEventListener('change', (ev) => {
-  handleChange()
-})
-
-btn.addEventListener('click', (ev) => {
-  handleKey('clip')
-})
-
-btn.addEventListener('keydown', (ev) => {
-  ev.preventDefault()
-  if (ev.key === 'Backspace') {
-    handleKey('clear')
-  } else if (ev.key === 'Tab') {
-    handleKey()
-  } else if (ev.key === 'Enter') {
-    handleKey('clip')
-  } else if (ev.key === ' ') {
-    handleKey('insert')
-  }
 })
